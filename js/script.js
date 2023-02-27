064e7823407b785877cdd2f08f2589168115d483
tinuola <24995224+tinuola@users.noreply.github.com>
2023-02-27 11:47:30 -0800

    Completes mobile layout

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const prevBtn = document.getElementById('prev')
 const nextBtn = document.getElementById('next')
-const images = document.querySelectorAll('.image')
+const images = document.querySelectorAll('.testimonial')
 
 let sliderLength = images.length
 let count = 0
