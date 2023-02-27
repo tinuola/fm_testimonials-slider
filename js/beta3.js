7026de5d6b0fdb7011f1fedb9b203c850a661329
tinuola <24995224+tinuola@users.noreply.github.com>
2023-02-27 10:40:00 -0800

    Refactors JS

--- js/beta3.js
+++ js/beta3.js
@@ -10,54 +10,34 @@ let nextImage;
 let previousImage;
 
 
-/* 
-  Target current image and remove visibility
-  Cue next image and add visibility
-  Set next image to be current image
-  Desaturate nextBtn at end of the slider
-*/
-const moveForward = () => {
+nextBtn.addEventListener('click', function(){
   if( count >= 0 && count < sliderLength-1){
     prevBtn.classList.remove('disable')
-
     currentImage = images[count]
     currentImage.classList.add('hide')
     nextImage = currentImage.nextElementSibling
     nextImage.classList.remove('hide')
     currentImage = nextImage
-    
     count++
   }
 
   if(count === sliderLength-1){
-    nextBtn.classList.add('disable')
+    this.classList.add('disable')
   }
-}
+})
 
 
-/* 
-  Remove visibility from current image
-  Cue previous image and add visibility
-  Set previous image to be current image
-  Desaturate prevBtn at start of the slider
-*/
-const moveBackward = () => {
+prevBtn.addEventListener('click', function(){
   if( count <= sliderLength && count > 0){
     nextBtn.classList.remove('disable')
-
     currentImage.classList.add('hide')
     prevImage = currentImage.previousElementSibling
     prevImage.classList.remove('hide')
     currentImage = prevImage
-
     count--;
   }
 
   if(count === 0){
-    prevBtn.classList.add('disable')
+    this.classList.add('disable')
   }
-}
-
-
-nextBtn.addEventListener('click',moveForward)
-prevBtn.addEventListener('click', moveBackward)
+})
